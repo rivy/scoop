ea8b0bd503af28566521ce4623c283ebbe8e84d1
Roy Ivy III <rivy.dev@gmail.com>
2016-01-10 18:32:42 -0600

    refactor Scoop-Manifest.Tests.ps1 to summarize and show only specific problems

--- test/Scoop-Manifest.Tests.ps1
+++ test/Scoop-Manifest.Tests.ps1
@@ -2,22 +2,41 @@
 . "$psscriptroot\..\lib\core.ps1"
 . "$psscriptroot\..\lib\manifest.ps1"
 
+$repo_dir = (Get-Item $MyInvocation.MyCommand.Path).directory.parent.FullName
+
 describe "manifest-validation" {
-    $bucketdir = "$psscriptroot\..\bucket\"
-    $manifest_files = get-childitem $bucketdir *.json
+    $bucketdir = "$repo_dir\bucket"
+    $files = get-childitem $bucketdir *.json
 
-    $manifest_files | foreach-object {
-        it "test validity of $_" {
-            $manifest = parse_json $_.fullname
+    $files_exist = ($files.Count -gt 0)
 
-            $url = arch_specific "url" $manifest "32bit"
-            if(!$url) {
-                $url = arch_specific "url" $manifest "64bit"
+    it $('manifest files exist ({0} found)' -f $files.Count) -skip:$(-not $files_exist) {
+        if (-not ($files.Count -gt 0))
+        {
+            throw "No manifest files were found"
+        }
+    }
+
+    it "manifest files are valid" -skip:$(-not $files_exist) {
+        $badFiles = @(
+            foreach ($file in $files)
+            {
+                try { $manifest = parse_json $file.fullname } catch { "[$($file.Name)]: Invalid json format"; continue }
+
+                $url = arch_specific "url" $manifest "32bit"
+                if(!$url) {
+                    $url = arch_specific "url" $manifest "64bit"
+                }
+
+                try { $null = $url | should not benullorempty } catch { "[$($file.Name)] `$url: "+$error[0].Exception.Message }
+                try { $null = $manifest | should not benullorempty } catch { "[$($file.Name)] `$manifest: "+$error[0].Exception.Message }
+                try { $null = $manifest.version | should not benullorempty } catch { "[$($file.Name)] `$manifest.version: "+$error[0].Exception.Message }
             }
+        )
 
-            $url | should not benullorempty
-            $manifest | should not benullorempty
-            $manifest.version | should not benullorempty
+        if ($badFiles.Count -gt 0)
+        {
+            throw "The following errors were found:`n$($badFiles -join "`n")"
         }
     }
 }
