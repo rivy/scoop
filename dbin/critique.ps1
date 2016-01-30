576d6cbd0c03e6f4f08cd3a4a43120dbc3ab56ad
Roy Ivy III <rivy.dev@gmail.com>
2016-01-30 17:47:11 -0600

    improve output of `critique`

--- dbin/critique.ps1
+++ dbin/critique.ps1
@@ -3,9 +3,9 @@ $rule_exclusions = @( 'PSAvoidUsingWriteHost' )
 
 ##
 
-$repo_dir = (Get-Item $MyInvocation.MyCommand.Path).directory.parent.FullName
+$repo_dir = (Get-Item $MyInvocation.MyCommand.Path).directory.parent
 
-$repo_files = @( Get-ChildItem $repo_dir -file -recurse -force )
+$repo_files = @( Get-ChildItem $repo_dir.fullname -file -recurse -force )
 
 $project_file_exclusions = @(
     $([regex]::Escape($repo_dir.fullname)+'\\.git\\.*$')
@@ -25,15 +25,32 @@ if (-not $files_exist) { throw "no files found to critique"}
 $lint_module_name = 'PSScriptAnalyzer'
 $lint_module_version = '1.3.0'
 if (-not (Get-Module $lint_module_name)) {
-    $filename = [System.IO.Path]::Combine($repo_dir,"vendor\$lint_module_name\$lint_module_version\$lint_module_name.psd1")
+    $filename = [System.IO.Path]::Combine($repo_dir.fullname,"vendor\$lint_module_name\$lint_module_version\$lint_module_name.psd1")
     import-module $(resolve-path $filename)
 }
 $have_delinter = Get-Module $lint_module_name
 
 if (-not $have_delinter) { throw "unable to find/load '$lint_module_name' for critique"}
 
-foreach ($file in $files) {
-    write-host -nonewline "Analyzing '$file' ... "
-    Invoke-ScriptAnalyzer $file.FullName -excluderule $rule_exclusions
-    write-host "done"
+function abbrev_message {
+    param([string]$message,[int]$size=$host.ui.RawUI.windowsize.width-1)
+    $result = $message
+    $continuation = '...'
+    $continuation_size = $continuation.length
+    if ($message.length -gt $size) { $result = $message.substring(0, $size-$continuation_size) + $continuation }
+    $result
 }
+
+if ($null -ne $files) { foreach ($file in $files) {
+    $repo_relative_name = [System.IO.Path]::Combine(($file.directoryname -replace $('^'+[regex]::escape($repo_dir.fullname)),'.'), $file.name)
+    write-host -nonewline "Analyzing $repo_relative_name ... "
+    $notes = Invoke-ScriptAnalyzer $file.FullName -excluderule $rule_exclusions
+    if ($notes.count -ne 0) {
+        write-output ''
+        foreach ($note in $notes) {
+            if ($note.severity -eq 'information') { write-host -fore cyan $(abbrev_message $("info:(@$($note.line)):'$($note.rulename)': $($note.message)")) }
+            if ($note.severity -eq 'warning') { write-host -fore darkyellow $(abbrev_message $("warn:(@$($note.line)):'$($note.rulename)': $($note.message)")) }
+            if ($note.severity -eq 'error') { write-host -fore red $(abbrev_message $("err!:(@$($note.line)):'$($note.rulename)': $($note.message)")) }
+        }
+    } else { write-host -fore green "done" }
+}}
