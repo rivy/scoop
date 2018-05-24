b873efa31c89c22f9fb77c1417b8c9e1b644a4e7
Roy Ivy III <rivy.dev@gmail.com>
2018-05-24 17:04:10 -0500

    fix ~ add Pester v3/4 compatibility to summarize-results

--- test/bin/summarize-results.ps1
+++ test/bin/summarize-results.ps1
@@ -11,7 +11,9 @@ if (-not (resolve-path $testresults_xml)) { throw "'$testresults_xml' not found"
 
 $indent_size = 3
 
-$tests = [xml](get-content (resolve-path $testresults_xml))
+# ref: [PowerShell XML Basics](ref: http://www.powershellmagazine.com/2013/08/19/mastering-everyday-xml-tasks-in-powershell)[`@`](https://archive.is/GfhiG)
+# NOTE: as XML gets larger it may be a noticeable improvement to use `$xml = new-object -typename XML; $xml.Load( ... )`
+$results = [xml](get-content (resolve-path $testresults_xml))
 
 function format-time( [decimal]$t ) {
     $unit = 's'
@@ -23,29 +25,30 @@ function format-time( [decimal]$t ) {
 }
 
 write-host "Test Summary"
-foreach ($t in $tests.'test-results'.'test-suite'.'results'.'test-suite') {
+$test_suites = $(Select-Xml -xml $results -xpath "//test-suite[results/test-case]").Node
+foreach ($s in $test_suites) {
     $c = 'red'
     $p = '[!] '
-    if ($t.success -eq 'true') {
+    if ($s.success -eq 'true') {
         $c = 'green'
         $p = '[+] '
     }
-    write-host -foreground $c $( "{0}{1}{2} ~ {3}" -f @($(' '*$indent_size*0), $p, $t.description, $(format-time $t.time)) )
-    if ($detailed  -or ($t.success -ne 'true') -or ($t.result -eq 'ignored')) {
-        foreach ($s in $t.results.'test-case') {
+    write-host -foreground $c $( "{0}{1}{2} ~ {3}" -f @($(' '*$indent_size*0), $p, $s.description, $(format-time $s.time)) )
+    if ($detailed  -or ($s.success -ne 'true') -or ($s.result -eq 'ignored')) {
+        foreach ($t in $s.results.'test-case') {
             $c = 'red'
             $p = '[!] '
-            if ($s.success -eq 'true') {
+            if ($t.success -eq 'true') {
                 $c = 'green'
                 $p = '[+] '
             }
-            if ($s.result -eq 'ignored') {
+            if ($t.result -eq 'ignored') {
                 $c = 'yellow'
                 $p = '[-] '
             }
-            write-host -foreground $c $( "{0}{1}{2} ~ {3}" -f @($(' '*$indent_size*1), $p, $s.description, $(format-time $s.time)) )
-            if ($s.success -ne 'true') {
-                $lines = [regex]::Split($s.failure.message,'(?m)\r?\n?$')
+            write-host -foreground $c $( "{0}{1}{2} ~ {3}" -f @($(' '*$indent_size*1), $p, $t.description, $(format-time $t.time)) )
+            if ($t.success -ne 'true') {
+                $lines = [regex]::Split($t.failure.message,'(?m)\r?\n?$')
                 foreach ($line in $lines) {
                     $line = [regex]::Replace($line, '\r\n|\r|\n', '')
                     if ($line -match '^\s*$') { continue; }
@@ -56,13 +59,13 @@ foreach ($t in $tests.'test-results'.'test-suite'.'results'.'test-suite') {
     }
 }
 
-$n_total = [int]$tests.'test-results'.total
-$n_failures = [int]$tests.'test-results'.failures
+$n_total = [int]$results.'test-results'.total
+$n_failures = [int]$results.'test-results'.failures
 $n_passed = $n_total - $n_failures
-$n_skipped = [int]$tests.'test-results'.ignored
+$n_skipped = [int]$results.'test-results'.ignored
 
 write-host -nonewline $("Total tests: {0} " -f $n_total)
 if ($n_passed -gt 0) { write-host -nonewline $("Passed: {0} " -f $n_passed) -foreground green }
 if ($n_failures -gt 0) { write-host -nonewline $("Failed: {0} " -f $n_failures) -foreground red }
 if ($n_skipped -gt 0) { write-host -nonewline $("(Skipped: {0}) " -f $n_skipped) -foreground yellow }
-write-host $("~ {0} " -f $(format-time $tests.'test-results'.'test-suite'.time))
+write-host $("~ {0} " -f $(format-time $results.'test-results'.'test-suite'.time))
